09c8a16800e5e1e4de3daa52c59a67972a9baa2b
Yvonne Ng <ywng@cern.ch>
2026-07-09 10:15:32 +0100

    DataPrep: cleaning up + adding dataprep

--- DataPrep.py
+++ DataPrep.py
@@ -1,58 +1,36 @@
-import requests
-import vaderSentiment
-
-import pandas as pd 
+import pandas as pd
 import numpy as np
-import requests
-
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-# USE the ISARCASM test and train csv files
 df = pd.read_csv("./iSarcasmEval/train/train.csv")
 
-# print empty columns
 df.isna().sum()
-
-# Drop all entries that have empty elements
 df = df.dropna(subset=["tweet", "sarcastic"])
 
-
-# VaderSentiment Example
-# --- examples -------
-#sentences = ["VADER is smart, handsome, and funny.",  # positive sentence example
-#             "VADER is smart, handsome, and funny!",  # punctuation emphasis handled correctly (sentiment intensity adjusted)
-#             "VADER is very smart, handsome, and funny.", # booster words handled correctly (sentiment intensity adjusted)
-#             "VADER is VERY SMART, handsome, and FUNNY.",  # emphasis for ALLCAPS handled
-#             "VADER is VERY SMART, handsome, and FUNNY!!!", # combination of signals - VADER appropriately adjusts intensity
-#             "VADER is VERY SMART, uber handsome, and FRIGGIN FUNNY!!!", # booster words & punctuation make this close to ceiling for score
-#             "VADER is not smart, handsome, nor funny.",  # negation sentence example
-#             "The book was good.",  # positive sentence
-#             "At least it isn't a horrible book.",  # negated negative sentence with contraction
-#             "The book was only kind of good.", # qualified positive sentence is handled correctly (intensity adjusted)
-#             "The plot was good, but the characters are uncompelling and the dialog is not great.", # mixed negation sentence
-#             "Today SUX!",  # negative slang with capitalization emphasis
-#             "Today only kinda sux! But I'll get by, lol", # mixed sentiment example with slang and constrastive conjunction "but"
-#             "Make sure you :) or :D today!",  # emoticons handled
-#             "Catch utf-8 emoji such as such as 💘 and 💋 and 😁",  # emojis handled
-#             "Not bad at all"  # Capitalized negation
-#             ]
-
-sentences = df['tweet']
-compls = []
 analyzer = SentimentIntensityAnalyzer()
-for sentence in sentences:
-    vs = analyzer.polarity_scores(sentence)
-	compls.append(vs['compound']) 
-   print("{:-<65} {}".format(sentence, str(vs)))
-df['compound'] = compls
 
-#positive sentiment: compound score >= 0.05
-#neutral sentiment: (compound score > -0.05) and (compound score < 0.05)
-#negative sentiment: compound score <= -0.05
+# Calculate only the compound score directly into the dataframe
+df['compound'] = df['tweet'].apply(lambda x: analyzer.polarity_scores(x)['compound'])
 
-df['positive'] = df['compound'] >= 0.05
+# Keep only strong negative (<= -0.2) and strong positive (>= 0.2)
+df = df[(df['compound'] <= -0.2) | (df['compound'] >= 0.2)]
 
-df['c'] = np.where(df['a'].map(len) > df['b'].map(len), df['a'].map(len), df['b'].map(len))
+print(df.head())
 
-df['pos'] = (df['compound'] >= 0.05).astype(int)
-df['neg'] = (df['compound'] <= -0.05.astype(int)
+
+#sentences = df['tweet']
+#compls = []
+
+#for sentence in sentences:
+#    vs = analyzer.polarity_scores(sentence)
+#    compls.append(vs['compound'])
+#    print("{:-<65} {}".format(sentence, str(vs)))
+#
+#df['compound'] = compls
+#
+#df['positive'] = df['compound'] >= 0.05
+#
+## df['c'] = np.where(df['a'].map(len) > df['b'].map(len), df['a'].map(len), df['b'].map(len)) # Removed: 'a' and 'b' are undefined
+#
+#df['pos'] = (df['compound'] >= 0.05).astype(int)
+#df['neg'] = (df['compound'] <= -0.05).astype(int)
